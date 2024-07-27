9ee625120a254ec49a2334f171ec2d2af70d42ab
Vĩnh Khiêm Nguyễn <ngvinhkhiem123@gmail.com>
2024-07-27 11:35:32 +0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const app = express()
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL
+    })
+)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+const PORT = process.env.PORT || 7777
+app.listen(5000, () => console.log(":::::SERVER READY ON " + PORT))
