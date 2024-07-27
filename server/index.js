6da3e594b4fe48c4840162cef3a57627013e870d
Vĩnh Khiêm Nguyễn <ngvinhkhiem123@gmail.com>
2024-07-27 12:10:38 +0700

    connect db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
+const dbconn = require('./config/dbconn')
 const app = express()
 app.use(
     cors({
@@ -9,6 +10,8 @@ app.use(
 )
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+dbconn()
 
 
 const PORT = process.env.PORT || 7777
